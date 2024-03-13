8ed6500b0654352728f5674b96a77fa3cd3be021
Fernando Daumichen <fernando.daumichen@gmail.com>
2024-03-13 16:01:41 -0700

    new loading

--- src/app/Leagues/page.tsx
+++ src/app/Leagues/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect, useCallback } from "react";
 import Image from "next/image";
-import CircularProgress from "@material-ui/core/CircularProgress";
+
 
 export default function Leagues() {
   return (
