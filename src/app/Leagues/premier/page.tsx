9b017c747a2360b7e04db3af326f5bd9b0111f3d
Fernando Daumichen <fernando.daumichen@gmail.com>
2024-03-15 15:42:16 -0700

    styling leagues

--- src/app/Leagues/premier/page.tsx
+++ src/app/Leagues/premier/page.tsx
@@ -2,30 +2,28 @@
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import  { useFetchBraA1,TeamStanding } from "../../actions/FetchLeagues";
+import { useFetchPremier, TeamStanding } from "../../actions/FetchLeagues";
 
-export default function BraA1() {
-  const { data, error } = useFetchBraA1();
+export default function Premier() {
+  const { data, error } = useFetchPremier();
 
-  if (error)
-    return <div className="text-red-500">Error loading data: {error}</div>;
+  if (error) return <div className="text-red-500">Error loading data: {error}</div>;
   if (!data) return <div>Loading...</div>;
 
-  // Correctly access the nested data for rendering
   const standings = data?.response?.[0]?.league?.standings[0];
   console.log(standings);
 
   if (!standings || !Array.isArray(standings)) {
     return <div>Invalid standings data</div>;
   }
+
   return (
     <div className="overflow-x-auto mt-6">
         <div className="flex justify-center">
-          <Image src="/brasileirao.png" alt="brasileirao" width={50} height={50} unoptimized={true} />
-      <h1 className="text-3xl md:text-xl text-white text-bold ">
-  Brasilerão Série A 
+            <Image src="/images/leagues/PremierLogo.png" alt="premierleague" width={150} height={150} unoptimized={true} />
+      <h1 className="text-8xl md:text-4xl text-white text-bold ">
+        Premier League Standings
       </h1>
-
         </div>
       {standings && (
         <div className="max-w-screen-lg mx-auto">
