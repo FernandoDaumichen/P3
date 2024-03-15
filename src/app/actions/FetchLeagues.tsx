7e9deb2272165e4a8754d9e2028c2ec782e22196
Fernando Daumichen <fernando.daumichen@gmail.com>
2024-03-15 01:51:25 -0700

    finally

--- src/app/actions/FetchLeagues.tsx
+++ src/app/actions/FetchLeagues.tsx
@@ -1,44 +1,40 @@
 import { useState, useEffect, useCallback } from 'react';
 import debounce from '@/utils/debounce';
-
-// Define interfaces for the expected API response structure
-interface Time {
-  time_id: number;
-  nome_popular: string;
-  sigla: string;
-  escudo: string;
+export interface TeamStanding {
+  position: number;
+  teamName: string;
+  playedGames: number;
+  wins: number;
+  draws: number;
+  losses: number;
+  points: number;
+  goalsFor: number;
+  goalsAgainst: number;
+  goalDifference: number;
 }
 
-interface GrupoItem {
-  posicao: number;
-  pontos: number;
-  time: Time;
-  jogos: number;
-  vitorias: number;
-  empates: number;
-  derrotas: number;
-  gols_pro: number;
-  gols_contra: number;
-  saldo_gols: number;
-  aproveitamento: number;
-  variacao_posicao: number;
-  ultimos_jogos: string[];
-}
-interface BraA1 {
-  "Campeonato Brasileiro": {
-    "gFase Única": GrupoItem[];
-  };
+export interface LeagueStandings {
+  season: string;
+  leagueName: string;
+  standings: TeamStanding[]; // An array of TeamStanding
 }
 
+export interface ApiResponse {
+  response: {
+    league: {
+      standings: TeamStanding[];
+    }
+  }[];
+}
 
-const useFetchBra = () => {
+const useFetchBraA1= () => {
     // Use the ApiResponse interface to type the data state
-    const [data, setData] = useState<BraA1 | null>(null);
+    const [data, setData] = useState<ApiResponse | null>(null);
     const [error, setError] = useState<string | null>(null);
 
     const fetchData = useCallback(async () => {
-        const url = "https://api.api-futebol.com.br/v1/campeonatos/10/fases/317 ";
-        const API_KEY ='live_af8d45c63929b1b080bde69442268d';
+      const url = 'https://api-football-beta.p.rapidapi.com/standings?season=2023&league=71';
+      const API_KEY = process.env.NEXT_PUBLIC_FOOTBALL_API;
         if (!API_KEY) {
           console.error('API key is not defined');
           setError('API key is not defined');
@@ -47,41 +43,36 @@ const useFetchBra = () => {
         const options = {
             method: "GET",
             headers: {
-              "Authorization": `Bearer ${API_KEY}`,
-          },
+                "X-RapidAPI-Key": API_KEY ,
+                "X-RapidAPI-Host": "api-football-beta.p.rapidapi.com",
+            },
         };
 
-       try {
-        const response = await fetch(url, options);
-        if (!response.ok) {
-            const errorMessage = response.status === 401 ? "Unauthorized: Check if the API key is correct."
-                            : response.status === 429 ? "Too Many Requests: You have hit the rate limit. Please try again later."
-                            : `HTTP error! status: ${response.status}`;
-            throw new Error(errorMessage);
+        try {
+            const response = await fetch(url, options);
+            if (!response.ok) {
+                const errorMessage = response.status === 401 ? "Unauthorized: Check if the API key is correct."
+                                : response.status === 429 ? "Too Many Requests: You have hit the rate limit. Please try again later."
+                                : `HTTP error! status: ${response.status}`;
+                throw new Error(errorMessage);
+            }
+            const result = await response.json();
+            // Ensure to validate or adjust this according to the actual structure of your API response
+            setData(result); 
+        } catch (error) {
+            if (error instanceof Error) {
+                setError(error.message);
+            }
         }
-        const result = await response.json();
-        // Ensure to validate or adjust this according to the actual structure of your API response
-        setData(result); 
-    } catch (error) {
-        if (error instanceof Error) {
-            setError(error.message);
-        }
-    }
-}, []);
+    }, []);
 
     useEffect(() => {
         const debouncedFetchData = debounce(fetchData, 1000);
         debouncedFetchData();
     }, [fetchData]);
 
+    
     return { data, error };
-//     return ( 
-//     <div>
-//       <h1>Api Status</h1>
-//       {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
-//       {error && <p>{error}</p>}
-//     </div>
-// )
 };
 
-export default useFetchBra;
+export default useFetchBraA1;
