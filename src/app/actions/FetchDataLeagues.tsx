f78e1f1957a8ce235b17bbed2b1871e3a0ddaf31
Fernando Daumichen <fernando.daumichen@gmail.com>
2024-03-23 18:47:27 -0700

    rest

--- src/app/actions/FetchDataLeagues.tsx
+++ src/app/actions/FetchDataLeagues.tsx
@@ -49,7 +49,7 @@ interface NewsItem {
     articles: Article[];
   }
   
-  const useFetchNewsDataBraA1 = () => {
+  export const useFetchNewsDataBraA1 = () => {
       const [data2, setData] = useState<ApiResponse | null>(null);
       const [error2, setError] = useState<string | null>(null);
   
@@ -77,6 +77,93 @@ interface NewsItem {
       return { data2, error2 };
   };
   
-  export default useFetchNewsDataBraA1;
+
+  //premier league
+  export const useFetchNewsDataPremier = () => {
+    const [data2, setData] = useState<ApiResponse | null>(null);
+    const [error2, setError] = useState<string | null>(null);
+
+    const fetchData = useCallback(async () => {
+        const url = "https://site.api.espn.com/apis/site/v2/sports/soccer/Eng.1/news";
+
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const result: ApiResponse = await response.json();
+            setData(result); 
+        } catch (error) {
+            if (error instanceof Error) {
+                setError(error.message);
+            }
+        }
+    }, []);
+
+    useEffect(() => {
+        fetchData();
+    }, [fetchData]);
+
+    return { data2, error2 };
+};
+
+//champions league
+export const useFetchNewsDataChampions = () => {
+    const [data2, setData] = useState<ApiResponse | null>(null);
+    const [error2, setError] = useState<string | null>(null);
+
+    const fetchData = useCallback(async () => {
+        const url = "https://site.api.espn.com/apis/site/v2/sports/soccer/uefa.champions/news";
+
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const result: ApiResponse = await response.json();
+            setData(result); 
+        } catch (error) {
+            if (error instanceof Error) {
+                setError(error.message);
+            }
+        }
+    }, []);
+
+    useEffect(() => {
+        fetchData();
+    }, [fetchData]);
+
+    return { data2, error2 };
+};
+
+//libertadores
+
+export const useFetchNewsDataLibertadores = () => {
+    const [data2, setData] = useState<ApiResponse | null>(null);
+    const [error2, setError] = useState<string | null>(null);
+
+    const fetchData = useCallback(async () => {
+        const url = "https://site.api.espn.com/apis/site/v2/sports/soccer/conmebol.libertadores/news";
+
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const result: ApiResponse = await response.json();
+            setData(result); 
+        } catch (error) {
+            if (error instanceof Error) {
+                setError(error.message);
+            }
+        }
+    }, []);
+
+    useEffect(() => {
+        fetchData();
+    }, [fetchData]);
+
+    return { data2, error2 };
+}
   
   
--- src/app/actions/FetchTopScores.tsx
+++ src/app/actions/FetchTopScores.tsx
@@ -183,3 +183,141 @@ export const useFetchBraA1TopScores= () => {
     // console.log(data3);
     return { data3, error3};
 };
+
+//premier league
+
+export const useFetchPremierTopScores= () => {
+    const [data3, setData] = useState<ApiResponse | null>(null);
+    const [error3, setError] = useState<string | null>(null);
+
+    const fetchData = useCallback(async () => {
+        const url = "https://api-football-beta.p.rapidapi.com/players/topscorers?season=2023&league=39";
+        const API_KEY = process.env.NEXT_PUBLIC_FOOTBALL_API;
+        if (!API_KEY) {
+          console.error('API key is not defined');
+          setError('API key is not defined');
+          return;
+      }
+        const options = {
+            method: "GET",
+            headers: {
+                "X-RapidAPI-Key": API_KEY ,
+                "X-RapidAPI-Host": "api-football-beta.p.rapidapi.com",
+            },
+        };
+
+        try {
+            const response = await fetch(url, options);
+            if (!response.ok) {
+                const errorMessage = response.status === 401 ? "Unauthorized: Check if the API key is correct."
+                                : response.status === 429 ? "Too Many Requests: You have hit the rate limit. Please try again later."
+                                : `HTTP error! status: ${response.status}`;
+                throw new Error(errorMessage);
+            }
+            const result = await response.json();
+            // Ensure to validate or adjust this according to the actual structure of your API response
+            setData(result); 
+        } catch (error) {
+            if (error instanceof Error) {
+                setError(error.message);
+            }
+        }
+    }, []);
+
+    useEffect(() => {
+        const debouncedFetchData = debounce(fetchData, 1000);
+        debouncedFetchData();
+    }, [fetchData]);
+
+    // console.log(data3);
+    return { data3, error3};
+}
+
+
+//Champions league
+export const useFetchChampionsTopScores= () => {
+const [data3, setData] = useState<ApiResponse | null>(null);
+const [error3, setError] = useState<string | null>(null);
+
+const fetchData = useCallback(async () => {
+    const url = "https://api-football-beta.p.rapidapi.com/players/topscorers?season=2023&league=2";
+    const API_KEY = process.env.NEXT_PUBLIC_FOOTBALL_API;
+    if (!API_KEY) {
+      console.error('API key is not defined');
+      setError('API key is not defined');
+      return;
+  }
+    const options = {
+        method: "GET",
+        headers: {
+            "X-RapidAPI-Key": API_KEY ,
+            "X-RapidAPI-Host": "api-football-beta.p.rapidapi.com",
+        },
+    };
+
+    try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+            const errorMessage = response.status === 401 ? "Unauthorized: Check if the API key is correct."
+                            : response.status === 429 ? "Too Many Requests: You have hit the rate limit. Please try again later."
+                            : `HTTP error! status: ${response.status}`;
+            throw new Error(errorMessage);
+        }
+        const result = await response.json();
+        // Ensure to validate or adjust this according to the actual structure of your API response
+        setData(result); 
+    } catch (error) {
+        if (error instanceof Error) {
+            setError(error.message);
+        }
+    }
+}, []);
+
+
+}
+
+//libertadores=113
+
+export const useFetchLibertadoresTopScores= () => {
+const [data3, setData] = useState<ApiResponse | null>(null);
+const [error3, setError] = useState<string | null>(null);
+
+const fetchData = useCallback(async () => {
+    const url = "https://api-football-beta.p.rapidapi.com/players/topscorers?season=2023&league=113";
+    const API_KEY = process.env.NEXT_PUBLIC_FOOTBALL_API;
+    if (!API_KEY) {
+      console.error('API key is not defined');
+      setError('API key is not defined');
+      return;
+  }
+    const options = {
+        method: "GET",
+        headers: {
+            "X-RapidAPI-Key": API_KEY ,
+            "X-RapidAPI-Host": "api-football-beta.p.rapidapi.com",
+        },
+    };
+
+    try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+            const errorMessage = response.status === 401 ? "Unauthorized: Check if the API key is correct."
+                            : response.status === 429 ? "Too Many Requests: You have hit the rate limit. Please try again later."
+                            : `HTTP error! status: ${response.status}`;
+            throw new Error(errorMessage);
+        }
+        const result = await response.json();
+        // Ensure to validate or adjust this according to the actual structure of your API response
+        setData(result); 
+    } catch (error) {
+        if (error instanceof Error) {
+            setError(error.message);
+        }
+    }
+}, []);
+
+useEffect(() => {
+    const debouncedFetchData = debounce(fetchData, 1000);
+    debouncedFetchData();
+}, [fetchData]);
+}
