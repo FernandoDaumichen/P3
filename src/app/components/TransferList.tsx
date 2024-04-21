54e5d65098842e0670c27c5f4b55c461fc5c51e1
Fernando Daumichen <fernando.daumichen@gmail.com>
2024-04-20 22:00:18 -0700

    transfers image

--- src/app/components/TransferList.tsx
+++ src/app/components/TransferList.tsx
@@ -1,18 +1,19 @@
-'use client';
-import React from 'react';
-import useFetchTransfers from '../actions/FetchTransfers';
-import useFetchImages from '../actions/FetchPlayersImage'; // Assuming you have a similar hook for fetching images
-import Image from 'next/image';
-import { BeatLoader } from 'react-spinners';
-
+"use client";
+import React from "react";
+import useFetchTransfers from "../actions/FetchTransfers";
+import useFetchImages from "../actions/FetchPlayersImage";
+import Image from "next/image";
+import { BeatLoader } from "react-spinners";
 
 const TransfersComponent = () => {
-  const { transfers, error: transferError } = useFetchTransfers();
-  const { images, error: imagesError } = useFetchImages(); // Fetch images similarly
+  const { transfers, error: transferError } = useFetchTransfers();    const playerIDs = transfers?.map(transfer => transfer.playerID) || [];
+  const { images, error: imagesError } = useFetchImages(playerIDs);
+
+
 
   // Helper function to convert timestamp to date string
   const formatDate = (timestamp: number): string => {
-    return new Date(timestamp * 1000).toLocaleDateString('en-US');
+    return new Date(timestamp * 1000).toLocaleDateString("en-US");
   };
 
   // Function to get image URL by playerID
@@ -21,10 +22,14 @@ const TransfersComponent = () => {
   };
 
   // Handle loading and error states
-  if (transferError || imagesError) return <div>Error: {transferError || imagesError}</div>;
-  if (transfers === null || images === null) return <div className="flex justify-center items-center h-[50vh]">
-  <BeatLoader color="#2e8ed7" loading={true} size={10} />
-</div>
+  if (transferError || imagesError)
+    return <div>Error: {transferError || imagesError}</div>;
+  if (transfers === null || images === null)
+    return (
+      <div className="flex justify-center items-center h-[50vh]">
+        <BeatLoader color="#2e8ed7" loading={true} size={10} />
+      </div>
+    );
 
   return (
     <div className="container mx-auto p-8">
@@ -33,16 +38,38 @@ const TransfersComponent = () => {
           <div key={transfer.id} className="w-full sm:w-1/2 md:w-1/3 px-2 mb-4">
             <div className="bg-gray-900 text-white rounded-lg shadow-md p-6">
               <div>
+                {getImageUrlByPlayerId(transfer.playerID) ? (
+                  <Image
+                    src={getImageUrlByPlayerId(transfer.playerID) || ""}
+                    alt={`Image of Player ${transfer.playerID}`}
+                    width={500}
+                    height={300}
+                    unoptimized={true}
+                    layout="responsive"
+                  />
+                ) : (
+                  <div>No Image Available</div>
+                )}
 
-                <h3 className="text-lg font-bold">Player ID: {transfer.playerID}</h3>
+                {/* <h3 className="text-lg font-bold">
+                  Player ID: {transfer.playerID}
+                </h3> */}
                 <p className="text-sm">From Club ID: {transfer.fromClubID}</p>
                 <p className="text-sm">To Club ID: {transfer.toClubID}</p>
-                <p className="text-sm">Transferred At: {formatDate(transfer.transferredAt)}</p>
+                <p className="text-sm">
+                  Transferred At: {formatDate(transfer.transferredAt)}
+                </p>
                 <p className="text-sm">Season: {transfer.season}</p>
               </div>
               <div>
-                <p className="text-lg font-bold">Transfer Fee: {transfer.transferFee.value} {transfer.transferFee.currency}</p>
-                <p className="text-sm">Market Value: {transfer.transferMarketValue.value} {transfer.transferMarketValue.currency}</p>
+                <p className="text-lg font-bold">
+                  Transfer Fee: {transfer.transferFee.value}{" "}
+                  {transfer.transferFee.currency}
+                </p>
+                <p className="text-sm">
+                  Market Value: {transfer.transferMarketValue.value}{" "}
+                  {transfer.transferMarketValue.currency}
+                </p>
               </div>
             </div>
           </div>
